f572be42136977ccf4b34565929fbe07f24aa4f7
SnyDrive25 <sunilgouvi@gmail.com>
2022-12-27 12:31:20 +0100

    get currencies

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const fetch = require('node-fetch');
+
+  const url = 'https://api.exchange.coinbase.com/currencies';
+  const options = {method: 'GET', headers: {accept: 'application/json'}};
+  
+  fetch(url, options)
+    .then(res => res.json())
+    .then(function(res) {
+        for(let crypto = 0; crypto < res.length; crypto++) {
+          if(res[crypto]['details']['type'] === 'crypto') {
+            var out = res[crypto]['id'];
+            document.getElementById("out").innerHTML += "<div class='case'>" + out + "</div>";
+          }
+        }
+      }
+    )
+    .catch(err => console.error('error:' + err));
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='part'>
+        <h1>Coinbase API</h1>
+        <div className='ctn flex' id="out">
+
+        </div>
+      </div>
     </div>
   );
 }
